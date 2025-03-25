dfbbd932c03149fe81240e4c7717b576a94ab738
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-03-25 14:06:44 +0530

    Equals Method

---
+++ src/com/jb/T19_JavaTips/EqualsRunner.java
@@ -0,0 +1,26 @@
+package com.jb.T19_JavaTips;
+class Client
+{
+    private int id;
+    public Client(int id) {
+    	super();
+        this.id = id;
+    }
+    
+}
+public class EqualsRunner {
+    public static void main(String[] args) {
+        Client c1 = new Client(1);
+        Client c2 = new Client(2);
+        Client c3 = new Client(1);
+        System.out.println(c1.equals(c2)); // inhert object class
+        System.out.println(c1.equals(c3));
+
+        
+    }
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+}
